a0dba3588a622485860b2dbb6e5975281a40b3c0
Pandaa <96748487+Pandaalifter@users.noreply.github.com>
2023-02-11 21:09:11 -0800

    sadical
    chadical

--- openwc-chad/src/openwc-chad.js
+++ openwc-chad/src/openwc-chad.js
@@ -27,7 +27,9 @@ class OpenwcChad extends LitElement {
     }
 
     .logo {
-      margin-top: 36px;
+      margin-top: 10px;
+      width: 100px;
+      height: 100px;
       animation: app-logo-spin infinite 20s linear;
     }
 
@@ -48,29 +50,142 @@ class OpenwcChad extends LitElement {
     .app-footer a {
       margin-left: 5px;
     }
+
+    .card{
+      border-radius: 24px;
+      border: 3px solid #041E42;
+      padding: 8px;
+      width: 400px;
+      display: block;
+      margin: auto;
+      position: relative;
+      overflow: hidden;
+      box-shadow: 0px 8px 24px #999;
+      background-color: lightgrey;
+      text-align: center;
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .textbox{
+      font-size: 15px;
+      margin-left: 112px;
+      text-align: right;
+      border-radius: 24px;
+    }
+    .description{
+      text-shadow: -1px 1px 2px #FFFFFF,
+                      1px 1px 2px #FFFFFF,
+                      1px -1px 0 #FFFFFF,
+                      -1px -1px 0 #FFFFFF;
+    }
+    .haxbtn{
+      display:inline-block;
+      text-align: center;
+      color: white;
+      background-color: #041E42;
+      border-radius: 5px;
+      font-size: 20px;
+      width: 120px;
+      height: 32px;
+      box-shadow: 0px 10px 24px #999;
+      border: 1px solid #041E42;
+      visibility: visible;
+      padding-top: 10px;
+      margin-right: 32px;
+    }
+    .haxbtn:hover{
+      color: #e0e0e0;
+      background-color: #07377a;
+      box-shadow: 0px 10px 24px white;
+      border: 1px solid #07377a;
+    }
+    .haxbtn:focus{
+      color: #e0e0e0;
+      background-color: #07377a;
+      box-shadow: 0px 10px 24px white;
+      border: 1px solid #07377a;
+    }
+    .title{
+      color: white;
+      font-size: 24px;
+      text-shadow: -1px 1px 2px #000,
+                      1px 1px 2px #000,
+                      1px -1px 0 #000,
+                      -1px -1px 0 #000;
+    }
+    .giaImg{
+      width: 200px;
+      border-radius: 24px;
+      box-shadow: 0px 10px 24px #999;
+      float: left;
+    }
+    .btnSpacing{
+      display: inline-block;
+    }
+    .outsideBtn{
+      color: white;
+      background-color: #041E42;
+      border-radius: 5px;
+      font-size: 20px;
+      width: 120px;
+      height: 50px;
+      box-shadow: 0px 10px 24px #999;
+      border: 1px solid #041E42;
+      margin-right: 32px;
+      margin-bottom: 16px;
+    }
+    .outsideBtn:hover{
+      color: #e0e0e0;
+      background-color: #07377a;
+      box-shadow: 0px 10px 24px white;
+      border: 1px solid #07377a;
+    }
+    .outsideBtn:focus{
+      color: #e0e0e0;
+      background-color: #07377a;
+      box-shadow: 0px 10px 24px white;
+      border: 1px solid #07377a;
+    }
+    .basic{
+      background-color: hotpink;
+    }
+    .hoverCard{
+      border: 3px solid purple;
+      box-shadow: 0px 8px 24px yellow;
+    }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = 'My Chadical App';
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
+        <div><img alt="open-wc logo" src=${logo} class="logo" /></div>
         <h1>${this.header}</h1>
-
-        <p>Edit <code>src/OpenwcChad.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
       </main>
+
+      <div class"btnSpacing">
+      <button class="outsideBtn">Duplicate</button>
+      <button class="outsideBtn">Background</button>
+      <button class="outsideBtn">Heading</button>
+      <button class="outsideBtn">Delete</button>
+      </div>
+
+      <div class="card">
+        <h1 class="title">Chad of Cyber IST</h1>
+
+        <img class="giaImg" src="https://media.discordapp.net/attachments/963095262363017246/1020131830323744788/unknown.png?width=468&height=468" alt="Professor Giacobe"/>
+
+        <div class="textbox">
+          <details>
+            <summary class="haxbtn">Details</summary>
+            <p class="description pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
+          </details>
+
+        </div>
+      </div>
 
       <p class="app-footer">
         🚽 Made with love by
